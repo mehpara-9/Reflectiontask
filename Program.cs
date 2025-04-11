4c85fac855b7d52784a1c681306f80270cfabc42
mehpara-9 <mehparefk-bb208@code.edu.az>
2025-04-11 13:48:39 +0400

    reflection

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+
+namespace Reflection.task
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Type usertype = typeof(User);
+            User user1 = (User)Activator.CreateInstance(usertype);
+
+            FieldInfo fieldId = usertype.GetField("id", BindingFlags.NonPublic  | BindingFlags.Instance);
+            fieldId.SetValue(user1,8);
+
+            FieldInfo fieldName = usertype.GetField("name", BindingFlags.NonPublic | BindingFlags.Instance);
+            fieldId.SetValue(user1, "Mehpare");
+
+            FieldInfo fieldSurname = usertype.GetField("surname", BindingFlags.NonPublic | BindingFlags.Instance);
+            fieldSurname.SetValue(user1, "Kicibeyova");
+
+            FieldInfo fieldAge = usertype.GetField("age", BindingFlags.NonPublic | BindingFlags.Instance |BindingFlags.Static);
+            fieldAge.SetValue(user1, 20);
+            
+
+            Console.WriteLine("ID:"+fieldId.GetValue);
+         //   Console.WriteLine("ad:"+fieldName.GetValue);
+           // Console.WriteLine("soyad:"+fieldSurname.GetValue);
+            Console.WriteLine("yas:" + fieldAge.GetValue);
+
+
+            MethodInfo method1 = usertype.GetMethod("GetFullName", BindingFlags.Public | BindingFlags.Instance);
+            method1.Invoke(user1 fieldId, fieldName, fieldSurname, fieldAge);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection.task
+{
+  public  class User
+    {
+        private string name;
+        private string surname;
+        private int  id;
+        private static int age;
+        public void GetFullName()
+        {
+            Console.WriteLine($"ad:{name},soyad:{surname}dir");
+        }
+        public static void ChangeAge(int age)
+        {
+            age =age;
+        }
+    }
+}
